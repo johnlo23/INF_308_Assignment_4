b34fc3dfa9ec6bb59ea0cba06c8a9dbcf6a3b4d8
John <john@jlny.org>
2022-09-13 00:30:18 -0400

    Moved major tasks to functions

--- main.py
+++ main.py
@@ -3,6 +3,39 @@
 # John Logiudice
 
 
+# Get list of items from text file
+def get_list_from_file(file_name):
+    file = open(file_name, 'r')
+    name_list = [readline.rstrip() for readline in file]
+    file.close()
+    name_list.sort()
+    return name_list
+
+
+# Print Library
+def print_library(books, available, borrowed):
+    # print Available and Borrowed lists side-by-side
+    print("Available books" + " " * 20 + "Borrowed books")
+    print("---------------" + " " * 20 + "--------------")
+
+    for i in range(0, len(books)):
+        print(f"{i+1}. ", end="")
+        # Available list may not have same number of items as book list
+        # so use try to handle index errors
+        try:
+            print(f"{available[i]}" + " "*((2-len(str(i+1)))+30-len(available[i])), end="")
+        except IndexError:
+            print(" "*((2-len(str(i+1)))+30), end="")
+
+        print(f"{i + 1}. ", end="")
+        # Borrowed list may not have same number of items as book list
+        # so use try to handle index errors
+        try:
+            print({borrowed[i]})
+        except IndexError:
+            print()
+
+
 # Welcome message
 welcome_message = 'The Classic Fiction Lending Library\n' \
     + '- ' * 20 + '\n' \
@@ -11,9 +44,7 @@ welcome_message = 'The Classic Fiction Lending Library\n' \
 print(welcome_message)
 
 # Get list of books from text file
-file = open('books.txt', 'r')
-book_list = [readline.rstrip() for readline in file]
-file.close()
+book_list = get_list_from_file("books.txt")
 
 # Create a copy of book_list into available_list
 available_list = book_list.copy()
@@ -21,14 +52,5 @@ available_list = book_list.copy()
 # Create empty list of borrowed books
 borrowed_list = list()
 
-# print Available and Borrowed lists side-by-side
-print("Available books" + " "*22 + "Borrowed books")
-print("---------------" + " "*22  + "--------------")
-
-for i in range(0, len(available_list)):
-    print(f"{i+1}. {available_list[i]}" + " "*((2-len(str(i+1)))+30-len(available_list[i])), end="")
-    print(f"{i + 1}. ", end="")
-    try:
-        print({borrowed_list[i]})
-    except IndexError:
-        print()
+# Print library
+print_library(book_list, available_list, borrowed_list)
